4f7bf1a8e4bf74c761c5d7a8a8221dec660236a3
Pau Casanellas <paucasanellas@icloud.com>
2022-10-13 18:26:16 +0200

    test: update AppNav tests

--- components/layout/navigation/__tests__/AppNav.test.ts
+++ components/layout/navigation/__tests__/AppNav.test.ts
@@ -1,5 +1,7 @@
 import { describe, expect, it } from 'vitest'
 import '@testing-library/jest-dom'
+import { fireEvent } from '@testing-library/vue'
+import { waitFor } from '@testing-library/dom'
 
 import { render } from '@/tests/render'
 import { router, routes } from '@/tests/router'
@@ -25,5 +27,24 @@ describe('NavbarDefault Component', () => {
       const link = component.getByRole('link', { name: route.title })
       expect(link).toBeInTheDocument()
     }
+  })
+
+  it('should navigate between all links', async () => {
+    for (const route of routes) {
+      await fireEvent.click(component.getByText(route.title))
+
+      await waitFor(() => {
+        const link = component.getByRole('link', { name: route.title })
+        expect(link).toHaveAttribute('aria-current', 'page')
+        expect(link).toHaveAttribute('href', route.path)
+      })
+    }
+  })
+
+  it('should be open when click toggle', async () => {
+    const button = component.getByRole('button', { class: /Toggle/i })
+    const aside = component.getByRole('complementary', { class: /AppNav/i })
+    await fireEvent.click(button)
+    expect(aside).toHaveClass('AppNav--open')
   })
 })
