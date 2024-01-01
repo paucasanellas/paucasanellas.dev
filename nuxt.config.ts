b01fe8687c0306001d1e9187e8c2bd66f16ab2aa
Pau Casanellas <paucasanellas@icloud.com>
2024-01-01 10:25:19 +0100

    feat(config): remove path prefix on components folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  components: [
+    { path: '~/components', pathPrefix: false }
+  ],
   devtools: { enabled: true }
 })
