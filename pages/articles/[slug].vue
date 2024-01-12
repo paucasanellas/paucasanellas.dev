a8419f7aa661fdb3744a860184a2f0e25682313f
Pau Casanellas <paucasanellas@icloud.com>
2024-01-12 10:54:37 +0100

    feat(article): fetch article data

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -2,10 +2,20 @@
   <main>
     <AppContainer>
       {{ params.slug }}
+      {{ article }}
     </AppContainer>
   </main>
 </template>
 
 <script lang="ts" setup>
+import type { Article } from '@/types'
+
+definePageMeta({
+  layout: 'sidebar'
+})
+
 const { params } = useRoute()
+const { locale } = useI18n()
+
+const article = await queryContent<Article>('articles', locale.value).where({ slug: params.slug }).findOne()
 </script>
